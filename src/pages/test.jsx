af02ab6433db30e044e27690953c21ccca01ae8b
Abdullah-saafi <abdullahsaafipro@gmail.com>
2026-04-21 12:22:57 +0500

    test

---
+++ src/pages/test.jsx
@@ -0,0 +1,7 @@
+export default Text(){
+  return(
+    <div className="">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus saepe repudiandae consectetur esse provident illum enim vero non ipsa exercitationem!</p>
+    </div>
+  )
+}
